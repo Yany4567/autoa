59e36f23e9694b8966bb577744ed35382242b561
lanou3g <lanou3g@lanou3gdeMac-mini-9.local>
2016-03-25 15:43:58 +0800

    1234

---
+++ 12qwq/12qwq/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  12qwq
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 高艳闯. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"1234");
+    
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
